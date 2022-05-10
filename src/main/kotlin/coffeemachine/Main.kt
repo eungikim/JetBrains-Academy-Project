8a9d53dedb156abe7b6c0a9bfa7c29517800e8ff
eungikim <eungikim.me@gmail.com>
2022-05-10 14:09:51 +0900

    Update estimate the number of servings

--- src/main/kotlin/coffeemachine/Main.kt
+++ src/main/kotlin/coffeemachine/Main.kt
@@ -1,9 +1,18 @@
 package coffeemachine
 
 fun main() {
-  val howManyCoffee = readln().toInt()
-  println("For $howManyCoffee cups of coffee you will need:")
-  println("${howManyCoffee * 200} ml of water")
-  println("${howManyCoffee * 50} ml of milk")
-  println("${howManyCoffee * 15} g of coffee beans")
+  val haveWater = readln().toInt()
+  val haveMilk = readln().toInt()
+  val haveCoffeeBean = readln().toInt()
+  val needCoffee = readln().toInt()
+  val canMakeCoffee = Math.min(Math.min(haveWater / 200, haveMilk / 50), haveCoffeeBean / 15)
+  println(
+    if (canMakeCoffee > needCoffee) {
+      "Yes, I can make that amount of coffee (and even ${canMakeCoffee - needCoffee} more than that)"
+    } else if (canMakeCoffee == needCoffee) {
+      "Yes, I can make that amount of coffee"
+    } else {
+      "No, I can make only $canMakeCoffee cups of coffee"
+    }
+  )
 }
