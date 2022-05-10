088c1e5356d08de29369d9b9737cd31f24b03bae
eungikim <eungikim.me@gmail.com>
2022-05-10 14:12:55 +0900

    Add buy,fill,take feature

--- src/main/kotlin/coffeemachine/Main.kt
+++ src/main/kotlin/coffeemachine/Main.kt
@@ -1,18 +1,68 @@
 package coffeemachine
 
+
+var haveWater = 400
+var haveMilk = 540
+var haveCoffeeBean = 120
+var haveDpCup = 9
+var haveMoney = 550
+
 fun main() {
-  val haveWater = readln().toInt()
-  val haveMilk = readln().toInt()
-  val haveCoffeeBean = readln().toInt()
-  val needCoffee = readln().toInt()
-  val canMakeCoffee = Math.min(Math.min(haveWater / 200, haveMilk / 50), haveCoffeeBean / 15)
-  println(
-    if (canMakeCoffee > needCoffee) {
-      "Yes, I can make that amount of coffee (and even ${canMakeCoffee - needCoffee} more than that)"
-    } else if (canMakeCoffee == needCoffee) {
-      "Yes, I can make that amount of coffee"
-    } else {
-      "No, I can make only $canMakeCoffee cups of coffee"
+  printState()
+
+  val operation = readLine()!!
+  when(operation) {
+    "buy" -> makeCoffee()
+    "fill" -> fillResource()
+    "take" -> takeMoney()
+  }
+
+  printState()
+}
+
+fun printState() {
+  println("The coffee machine has:")
+  println("$haveWater ml of water")
+  println("$haveMilk ml of milk")
+  println("$haveCoffeeBean g of coffee beans")
+  println("$haveDpCup disposable cups")
+  println("\$$haveMoney of money")
+}
+
+fun makeCoffee() {
+  val selectedMenu = readLine()!!.toInt()
+  when (selectedMenu) {
+    1 -> {
+      haveWater -= 250
+      haveCoffeeBean -= 16
+      haveDpCup--
+      haveMoney += 4
     }
-  )
+    2 -> {
+      haveWater -= 350
+      haveMilk -= 75
+      haveCoffeeBean -= 20
+      haveDpCup--
+      haveMoney += 7
+    }
+    3 -> {
+      haveWater -= 200
+      haveMilk -= 100
+      haveCoffeeBean -= 12
+      haveDpCup--
+      haveMoney += 6
+    }
+  }
+}
+
+fun fillResource() {
+  haveWater += readLine()!!.toInt()
+  haveMilk += readLine()!!.toInt()
+  haveCoffeeBean += readLine()!!.toInt()
+  haveDpCup += readLine()!!.toInt()
+}
+
+fun takeMoney() {
+  println("I gave you \$$haveMoney")
+  haveMoney = 0
 }
